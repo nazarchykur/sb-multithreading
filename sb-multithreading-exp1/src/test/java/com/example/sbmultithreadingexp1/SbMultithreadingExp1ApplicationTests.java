b41975538074c28a4f41e04db1794f464a4d8cdd
nazarchykur <nazarchykur@gmail.com>
2023-02-20 16:41:43 +0200

    added example 1: Multithreading in Spring Boot using CompletableFuture + @Async

---
+++ sb-multithreading-exp1/src/test/java/com/example/sbmultithreadingexp1/SbMultithreadingExp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbmultithreadingexp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMultithreadingExp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
