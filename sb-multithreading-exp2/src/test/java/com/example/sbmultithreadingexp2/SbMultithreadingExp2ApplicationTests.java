48b01600d1a6dfc70195779d6c438fadc9876a18
nazarchykur <nazarchykur@gmail.com>
2023-02-21 19:07:57 +0200

    added example with Redis Rqueue

---
+++ sb-multithreading-exp2/src/test/java/com/example/sbmultithreadingexp2/SbMultithreadingExp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbmultithreadingexp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMultithreadingExp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
